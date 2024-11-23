1d40d88887e55d2c803a00085a0b20199ac72aca
Kol.ds <fatbahad@gmail.com>
2024-11-23 23:33:54 +0300

    88. Merge Sorted Array

--- LeetCode/Main.cpp
+++ LeetCode/Main.cpp
@@ -1,71 +1,110 @@
 
 #include <vector>
 
-int BinarySearch(const std::vector<int>& SortedArray, const int& Target)
-{
-	int Start = 0;
-	int End = SortedArray.size() - 1;
+//
+// int BinarySearch(const std::vector<int>& SortedArray, const int& Target)
+// {
+// 	int Start = 0;
+// 	int End = SortedArray.size() - 1;
+//
+// 	while (Start <= End)
+// 	{
+// 		int Mid = Start + (End - Start) / 2;
+// 		if (SortedArray[Mid] == Target) return Mid;
+//
+// 		if (SortedArray[Mid] > Target)
+// 		{
+// 			End = Mid - 1;
+// 		}
+// 		else if (SortedArray[Mid] < Target)
+// 		{
+// 			Start = Mid + 1;
+// 		}
+// 	}
+// 	
+// }
+//
+//
+//
+//
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+//  
+// class Solution
+// {
+// public:
+// 	ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
+// 	{  
+// 		ListNode* dummy = new ListNode(0);  
+// 		ListNode* current = dummy;  
+// 		
+// 		while (list1 != nullptr && list2 != nullptr)
+// 		{  
+// 			if (list1->val <= list2->val)
+// 			{  
+// 				current->next = list1;  
+// 				list1 = list1->next;  
+// 			} else {  
+// 				current->next = list2;  
+// 				list2 = list2->next;  
+// 			}  
+// 			current = current->next;  
+// 		}  
+// 		
+// 		if (list1 != nullptr) {  
+// 			current->next = list1;  
+// 		}  
+//         
+// 		if (list2 != nullptr) {  
+// 			current->next = list2;  
+// 		}  
+//
+// 		return dummy->next;  
+// 	}
+// };
 
-	while (Start <= End)
-	{
-		int Mid = Start + (End - Start) / 2;
-		if (SortedArray[Mid] == Target) return Mid;
-
-		if (SortedArray[Mid] > Target)
-		{
-			End = Mid - 1;
-		}
-		else if (SortedArray[Mid] < Target)
-		{
-			Start = Mid + 1;
-		}
-	}
-	
-}
-
-
-
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
- 
-class Solution
-{
+class Solution {
 public:
-	ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
-	{  
-		ListNode* dummy = new ListNode(0);  
-		ListNode* current = dummy;  
-		
-		while (list1 != nullptr && list2 != nullptr)
-		{  
-			if (list1->val <= list2->val)
-			{  
-				current->next = list1;  
-				list1 = list1->next;  
-			} else {  
-				current->next = list2;  
-				list2 = list2->next;  
-			}  
-			current = current->next;  
-		}  
-		
-		if (list1 != nullptr) {  
-			current->next = list1;  
-		}  
-        
-		if (list2 != nullptr) {  
-			current->next = list2;  
-		}  
+	void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
+		std::vector<int> Result;
+		Result.reserve(m + n);
+		int IndexM = 0;
+		int IndexN = 0;
+		while (m > IndexM  && n > IndexN)
+		{
+			if (nums1[IndexM] < nums2[IndexN])
+			{
+				Result.push_back(nums1[IndexM++]);
+			}
+			else
+			{
+				Result.push_back(nums2[IndexN++]);
+			}
+		}
 
-		return dummy->next;  
+		if (IndexM == m)
+		{
+			for (int i = IndexN; i < n; i++)
+			{
+				Result.push_back(nums2[i]);
+			}
+		}
+		if (IndexN == n)
+		{
+			for (int i = IndexM; i < m; i++)
+			{
+				Result.push_back(nums1[i]);
+			}
+		}
+		nums1 = Result;
 	}
 };
+
 
 int main()
 {
